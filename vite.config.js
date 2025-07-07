8aed87295ac7a5d8ab58e1dc9ead43840c971e81
krushal29 <krushalbhadiyadra2908@gmail.com>
2025-07-07 23:22:43 +0530

    created

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),visualizer()],
+  build: {
+    chunkSizeWarningLimit: 1000 // set to 1000kb or any value that fits your need
+  }
 })
